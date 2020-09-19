7e865d9cfa241878dd5269e69a39929a511255a8
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-19 15:13:13 +0530

    Add files via upload

--- Stack using Linked List/main.c
+++ Stack using Linked List/main.c
@@ -46,5 +46,61 @@ int main()
     run();
     return 0;
 }
-
 // _______________________ END of Main Function __________
+
+void run()
+{
+    int choice,data;
+    printf("\nWelcome to Stack operation program using Linked List\n");
+
+    while(choice!=0)
+    {
+        choice=menu();
+        switch(choice)
+        {
+        case 0:
+            break;
+
+        case 1:
+            printf("\nEnter the data = ");
+            scanf("%d",&data);
+            //push(data);
+            break;
+
+        case 2:
+            //data=pop();
+            printf("\nItem popped = %d\n",data);
+            break;
+
+        case 3:
+            //data=showTop();
+            if(data==-999)
+                printf("\nStack is Empty\n");
+            else
+                printf("\nItem at the top is = %d\n",data);
+            break;
+
+        case 4:
+            //display_stack();
+            break;
+
+        default:
+            printf("\nWrong Choice..Try again\n");
+        }
+    }
+    return 0;
+}
+
+int menu()
+{
+    int choice;
+    printf("\nList of Choices\n");
+    printf("\n Push\t\t\t= 1\n");
+    printf("\n Pop\t\t\t= 2 \n");
+    printf("\n Show Top\t\t= 3\n");
+    printf("\n Display Stack\t\t= 4\n");
+    printf("\n Exit\t\t\t= 0\n");
+    printf("\nEnter your choice = ");
+    scanf("%d",&choice);
+    return choice;
+}
