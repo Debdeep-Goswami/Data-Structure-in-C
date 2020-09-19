6b04556b398f5454c46337cfb3bf5f656acb5921
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-19 15:06:59 +0530

    Add files via upload

--- Stack using Linked List/main.c
+++ Stack using Linked List/main.c
@@ -1,1 +1,50 @@
 
+/**********************************************
+        Stack using Linked List
+***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/******** Global variable declarations *********************/
+
+typedef struct node
+{
+    int data;
+    struct node * link;
+}NODE;
+
+NODE*head=NULL; //  This will work as the top of the stack
+
+/*********************************************************/
+
+
+/************************ Used Functions *****************/
+
+//--------------------- Necessory for Stack Operation -------
+
+void push(int);
+
+int pop();
+
+int showTop();
+
+// -------------------- Other utility functions --------------
+
+void run();
+
+void display_stack();
+
+int menu();
+
+/********************************************************/
+
+//_________________________ Main Function ________________
+
+int main()
+{
+    run();
+    return 0;
+}
+
+// _______________________ END of Main Function __________
