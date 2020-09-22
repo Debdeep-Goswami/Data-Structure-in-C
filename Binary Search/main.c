acd7b29380a274c1e876538046fe12d9d1b10c03
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-22 16:53:48 +0530

    Structure Created

---
+++ Binary Search/main.c
@@ -0,0 +1,40 @@
+/**********************************************
+            Binary Search
+***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 100
+
+/************************ Prototype of the Functions *****************/
+
+void getData(int *,int);
+
+void generateData(int *,int);
+
+void sortData(int*,int);
+
+int search(int*,int,int);
+
+void display(int*,int);
+
+// _____________________ Utility Functions ________________
+
+int menu();
+
+void run();
+
+/********************************************************/
+
+//________________________ Main Function ________________
+
+int main()
+{
+    run();
+    return 0;
+}
+
+//__________________END of Main Function ________________
+
+
