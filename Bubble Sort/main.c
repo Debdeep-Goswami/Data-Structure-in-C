86f0ee868895728f8b9b8647587565338b50bd11
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-23 18:56:36 +0530

    Running Successfully

--- Bubble Sort/main.c
+++ Bubble Sort/main.c
@@ -102,3 +102,68 @@ void display(int *a,int no_of_elements)
     printf("\n");
 }
 
+int menu()
+{
+    int choice=-999;
+
+    printf("\n______________________________________\n");
+    printf("\n********* Bubble Sort ***************");
+    printf("\n______________________________________\n");
+
+    printf("\n List of Choices\n");
+    printf("\n Insert Data Manually\t\t\t = 1");
+    printf("\n Insert Data Randomly\t\t\t = 2");
+    printf("\n Display Data array\t\t\t = 3");
+    printf("\n Bubble Sort in Ascending Order\t\t = 4");
+    printf("\n Bubble Sort in Descending Order\t = 5");
+    printf("\n Exit\t\t\t\t\t = 0");
+    printf("\n\n Enter your Choice = ");
+    scanf("%d",&choice);
+
+    system("cls");
+    return choice;
+}
+
+void run()
+{
+    int data_array[MAX],no_of_elements,search_element,choice=-999,position;
+    while(choice!=0)
+    {
+        choice=menu();
+        switch(choice)
+        {
+        case 0:
+            break;
+
+        case 1:
+            printf("\nEnter the number of elements to be inserted manually = ");
+            scanf("%d",&no_of_elements);
+            getData(data_array,no_of_elements);
+            break;
+
+        case 2:
+            printf("\nEnter the number of elements to be inserted randomly = ");
+            scanf("%d",&no_of_elements);
+            generateData(data_array,no_of_elements);
+            break;
+
+        case 3:
+            display(data_array,no_of_elements);
+            break;
+
+        case 4:
+            bubble_sort_ascending(data_array,no_of_elements);
+            break;
+
+        case 5:
+            bubble_sort_descending(data_array,no_of_elements);
+            break;
+
+        default:
+            printf("\nWrong Choice.. Try Again..\n");
+
+        }
+    }
+}
+
+/***************  End of Function Definitions  ****************/
