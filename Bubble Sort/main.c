b778696ab23f10e390863b0dfcfdd50960ba5fe6
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-23 18:42:17 +0530

    Descending Bubble Sort function added

--- Bubble Sort/main.c
+++ Bubble Sort/main.c
@@ -75,3 +75,21 @@ void bubble_sort_ascending(int *a,int no_of_elements)
         }
     }
 }
+
+void bubble_sort_descending(int *a,int no_of_elements)
+{
+    int temp,i,j;
+    for(i=0;i<no_of_elements-1;i++)
+    {
+        for(j=0;j<(no_of_elements-1)-i;j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+
