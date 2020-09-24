3415a904e043644af7aef43d92b46d416af4662d
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-24 22:26:46 +0530

    Structure Done

---
+++ Queue using Array/main.c
@@ -0,0 +1,51 @@
+
+/**********************************************
+        Queue using Array
+***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+/******** Global variable declarations *********************/
+
+int Front=0;
+
+int Rear=0;
+
+int Queue_Size;
+
+int Queue[MAX];
+
+
+
+/*********************************************************/
+
+
+/************************ Prototype of the Functions *****************/
+
+void enqueue(int);
+
+int dequeue();
+
+void display_queue();
+
+// _____________________ Utility Functions ________________
+
+void run();
+
+int menu();
+
+/********************************************************/
+
+//_________________________ Main Function ________________
+
+int main()
+{
+    run();
+    return 0;
+}
+
+// _______________________ END of Main Function __________
+
+
