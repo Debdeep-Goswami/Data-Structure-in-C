4f3e643af6cd516d3c1b0e0767e90e7e9aca13be
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-25 23:01:36 +0530

    Executing with 1st element error in output array

--- Quick Sort ( Recursive )/main.c
+++ Quick Sort ( Recursive )/main.c
@@ -142,3 +142,42 @@ int menu()
     return choice;
 }
 
+void run()
+{
+    int choice=-999;
+    while(choice!=0)
+    {
+        choice=menu();
+        switch(choice)
+        {
+        case 0:
+            break;
+
+        case 1:
+            printf("\nEnter the number of elements to be inserted manually = ");
+            scanf("%d",&No_of_elements);
+            getData();
+            break;
+
+        case 2:
+            printf("\nEnter the number of elements to be inserted randomly = ");
+            scanf("%d",&No_of_elements);
+            generateData();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        case 4:
+            quick_sort(0,No_of_elements);
+            break;
+
+        default:
+            printf("\nWrong Choice.. Try Again..\n");
+
+        }
+    }
+}
+
+/***************  End of Function Definitions  ****************/
