bf36ad10d0ee1aa887e908f8e6091224c7ec6a00
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-26 07:15:55 +0530

    Display Function added

--- Selection Sort/main.c
+++ Selection Sort/main.c
@@ -93,3 +93,12 @@ void selection_sort_descending(int *a,int no_of_elements)
     }
 }
 
+void display(int *a,int no_of_elements)
+{
+    int i;
+    printf("\nThe elements in the array :-\n");
+    for(i=0;i<no_of_elements;i++)
+        printf("  %d",a[i]);
+    printf("\n");
+}
+
