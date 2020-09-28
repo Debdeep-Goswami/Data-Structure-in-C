24d11b7990d0f60413952464bb2804fb68f3e578
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-09-28 22:47:50 +0530

    Structure Done

---
+++ Modified Bubble Sort/main.c
@@ -0,0 +1,40 @@
+/**********************************************
+            Modified Bubble Sort
+***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 100
+
+/************************ Prototype of the Functions *****************/
+
+void getData(int *,int);
+
+void generateData(int *,int);
+
+void modified_bubble_sort_ascending(int*,int);
+
+void modified_bubble_sort_descending(int*,int);
+
+void display(int*,int);
+
+// _____________________ Utility Functions ________________
+
+int menu();
+
+void run();
+
+/********************************************************/
+
+//________________________ Main Function ________________
+
+int main()
+{
+    run();
+    return 0;
+}
+
+//__________________END of Main Function ________________
+
+
